d692e6ab34306cc9699fb3bfcf84c67682758a49
Irfan <irfan.arif@devsinc.com>
2024-02-15 18:38:24 +0500

    Added boilerplate code

---
+++ src/main/java/com/example/AsyncApiGetCall/AsyncApiGetCallApplication.java
@@ -0,0 +1,13 @@
+package com.example.AsyncApiGetCall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncApiGetCallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncApiGetCallApplication.class, args);
+	}
+
+}
