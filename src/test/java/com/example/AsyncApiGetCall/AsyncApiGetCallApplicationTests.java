52a3b6227506d5a8c8f34217ff4d616b64b11770
Irfan <irfan.arif@devsinc.com>
2024-02-15 18:38:24 +0500

    Added boilerplate code

---
+++ src/test/java/com/example/AsyncApiGetCall/AsyncApiGetCallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AsyncApiGetCall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncApiGetCallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
